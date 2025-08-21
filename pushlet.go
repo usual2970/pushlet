ca284a979c40535f879e34b423ba76e0100df7c5
Yoan.liu <usual2970@gmail.com>
2025-08-21 18:03:36 +0800

    add heartbeat

--- pushlet.go
+++ pushlet.go
@@ -3,18 +3,26 @@ package pushlet
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 // Pushlet 是消息推送系统的主要入口点
 type Pushlet struct {
-	broker *Broker
+	broker            *Broker
+	heartbeatInterval time.Duration // 心跳间隔
 }
 
 // New 创建一个新的 Pushlet 实例
 func New() *Pushlet {
 	return &Pushlet{
-		broker: NewBroker(),
+		broker:            NewBroker(),
+		heartbeatInterval: 30 * time.Second, // 默认30秒心跳
 	}
+}
+
+// SetHeartbeatInterval 设置心跳间隔
+func (p *Pushlet) SetHeartbeatInterval(interval time.Duration) {
+	p.heartbeatInterval = interval
 }
 
 // EnableDistributedMode 启用分布式模式
@@ -35,7 +43,6 @@ func (p *Pushlet) Stop() {
 // HandleSSE 处理 SSE 连接请求
 // 路径参数可以用于区分不同的主题
 func (p *Pushlet) HandleSSE(w http.ResponseWriter, r *http.Request) {
-
 	// 检查请求方法
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -47,6 +54,7 @@ func (p *Pushlet) HandleSSE(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Cache-Control")
 
 	// 获取请求路径作为主题
 	topic := r.URL.Query().Get("topic")
@@ -67,28 +75,52 @@ func (p *Pushlet) HandleSSE(w http.ResponseWriter, r *http.Request) {
 	client.SendMessage(NewMessage("connected", "Connection established"))
 	log.Println("Connection message sent to client:", client.ID, "topic:", topic)
 
-	// 保持连接打开直到客户端断开
+	// 获取请求上下文
 	ctx := r.Context()
 	flusher := w.(http.Flusher)
 
-	// 监听来自客户端的消息
-	go func() {
-		for msg := range client.Send {
+	// 创建心跳定时器
+	heartbeatTicker := time.NewTicker(p.heartbeatInterval)
+	defer heartbeatTicker.Stop()
+
+	// 监听消息、心跳和断开连接
+	for {
+		select {
+		case msg, ok := <-client.Send:
+			if !ok {
+				// 客户端通道已关闭
+				log.Println("Client channel closed:", client.ID, "topic:", topic)
+				return
+			}
+
 			// 写入 SSE 格式的消息
 			eventStr := "event: " + msg.Event + "\n"
 			dataStr := "data: " + msg.Data + "\n\n"
 
 			_, err := w.Write([]byte(eventStr + dataStr))
 			if err != nil {
+				log.Println("Error writing to client:", client.ID, "error:", err)
 				return
 			}
 			flusher.Flush()
-		}
-	}()
 
-	// 等待客户端断开连接
-	<-ctx.Done()
-	log.Println("Client disconnected:", client.ID, "topic:", topic)
+		case <-heartbeatTicker.C:
+			// 发送心跳注释行
+			heartbeatMsg := ": heartbeat " + time.Now().Format("2006-01-02 15:04:05") + "\n\n"
+			_, err := w.Write([]byte(heartbeatMsg))
+			if err != nil {
+				log.Println("Error sending heartbeat to client:", client.ID, "error:", err)
+				return
+			}
+			flusher.Flush()
+			log.Printf("Heartbeat sent to client: %s, topic: %s", client.ID, topic)
+
+		case <-ctx.Done():
+			// 客户端断开连接
+			log.Println("Client disconnected:", client.ID, "topic:", topic)
+			return
+		}
+	}
 }
 
 // Publish 向指定主题发布消息
